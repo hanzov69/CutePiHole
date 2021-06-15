e4c267c92ead0cf6dcbde3678fa22c41224a4489
hanzov69 <hanzo@freezerpants.com>
2021-06-14 22:13:05 -0400

    Initial commit
    basic functionality

---
+++ README.md
@@ -0,0 +1,50 @@
+# CutePiHole
+
+(WIP)
+
+## Assumptions:
+
+You have a Raspberry Pi of some flavor, using a [WaveShare 1.44inc LCD HAT](https://www.waveshare.com/wiki/1.44inch_LCD_HAT)
+
+You have installed Raspbian, and [Installed PiHole](https://github.com/pi-hole/pi-hole/#one-step-automated-install)
+
+## Install
+1. Follow the guide found here: https://www.waveshare.com/wiki/1.44inch_LCD_HAT
+2. Setup Python:
+    sudo apt-get update
+    sudo apt-get install python3-pip 
+    sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+    sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
+    sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
+    sudo pip install pillow
+    sudo apt-get install libopenjp2-7 libtiff5 python3-numpy ttf-dejavu python3-requests
+
+    Note: Apt is preferred as the wheels take forever to install
+3. Clone this repo to your home directory
+4. Rename `cutepihole.ini.sample` to `cutepihole.ini`
+5. `cat /etc/pihole/setupVars.conf`, copy the value of `WEBPASSWORD` to `pihole_api_pass` in the config file
+6. Register for an OpenWeatherMap account, get an API Key. Save this to `owm_api_key` in the config file
+
+## Usage
+`python cutepihole.py`
+
+Scroll through screens by pressing UP/DOWN on left joystick
+Temporarily display system stats with Button 1
+Disable PiHole blocking for 5 minutes (default) with Button 2
+Turn Off/On the display with Button 3
+
+## Configuration Options
+`owm_api_key` - see above
+`fixed_location` - if you don't want CutePiHole to detect your location automatically
+`location` - written automatically if above is `false`. Otherwise, set to Lat,Lon
+`interval` - the frequency OpenWeatherMap is hit. Default is 60s, increase if you hit API rate limits, this doesn't need a livestream
+
+`default_panel` - choose between `weather`,`stats`,`pihole`
+
+`pihole_api_url` - don't change this, unless you know what you're doing
+`pihole_api_pass` - see above
+`pihole_disable_time` - duration to temporarily suspend pihole blocking, time is in seconds
+`pihole_config_file` - no function currently
+
+`update_panel` - no function currently
+`debug` - displays debug information on console
---
+++ cutepihole.py
@@ -0,0 +1,323 @@
+# -*- coding: utf-8 -*-
+import requests
+import json
+import re
+import subprocess
+import configparser
+import time
+import signal
+import sys
+
+import RPi.GPIO as GPIO
+
+# Import Python Imaging Library
+from PIL import Image, ImageDraw, ImageFont
+
+# Import LCD functions
+import LCD_1in44
+import LCD_Config
+
+# Get some GPIO pins sorted
+KEY_UP_PIN     = 6 
+KEY_DOWN_PIN   = 19
+KEY_LEFT_PIN   = 5
+KEY_RIGHT_PIN  = 26
+KEY_PRESS_PIN  = 13
+KEY1_PIN       = 21
+KEY2_PIN       = 20
+KEY3_PIN       = 16
+
+#init GPIO
+GPIO.setmode(GPIO.BCM) 
+GPIO.cleanup()
+GPIO.setup(KEY_UP_PIN,      GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Input with pull-up
+GPIO.setup(KEY_DOWN_PIN,    GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
+GPIO.setup(KEY_LEFT_PIN,    GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Input with pull-up
+GPIO.setup(KEY_RIGHT_PIN,   GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(KEY_PRESS_PIN,   GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(KEY1_PIN,        GPIO.IN, pull_up_down=GPIO.PUD_UP)      # Input with pull-up
+GPIO.setup(KEY2_PIN,        GPIO.IN, pull_up_down=GPIO.PUD_UP)      # Input with pull-up
+GPIO.setup(KEY3_PIN,        GPIO.IN, pull_up_down=GPIO.PUD_UP)      # Input with pull-up
+
+#parse our config
+config = configparser.ConfigParser()
+config.read('cutepihole.ini')
+
+#weather config
+varApiKey            = config['weather']['owm_api_key']
+varFixedLocation     = config['weather']['fixed_location']
+varLocation          = config['weather']['location']
+#varInterval          = config['weather'].getint('interval')
+varInterval          = 0
+
+#pihole config
+varPiholeApiUrl      = config['pihole']['pihole_api_url']
+varPiholeApiPass     = config['pihole']['pihole_api_pass']
+varPiholeDisableTime = config['pihole']['pihole_disable_time']
+varPiholeConfigFile  = config['pihole']['pihole_config_file']
+#panels config
+varDefaultPanel      = config['panels']['default_panel']
+if varDefaultPanel == "pihole":
+    screenid = 1
+elif varDefaultPanel == "weather":
+    screenid = 2
+elif varDefaultPanel == "stat":
+    screenid = 3
+
+#app config
+varUpdatePanel       = config['app']['update_panel']
+varDebug             = config['app']['debug']
+
+
+
+# url to disable pihole
+disable_url = varPiholeApiUrl + "?disable=" + varPiholeDisableTime + "&auth=" + varPiholeApiPass
+
+# catch those SIGINTs
+def signal_handler(sig, frame):
+    GPIO.output(LCD_Config.LCD_BL_PIN,GPIO.LOW)
+    print('Caught SIGINT')
+    sys.exit(0)
+
+#get location based on IP
+def ipInfo(addr=''):
+    from urllib.request import urlopen
+    from json import load
+    if addr == '':
+        url = 'https://ipinfo.io/json'
+    else:
+        url = 'https://ipinfo.io/' + addr + '/json'
+    res = urlopen(url)
+    #response from url(if res==None then check connection)
+    data = load(res)
+    config['weather']['location'] = data['loc']
+    with open('cutepihole.ini', 'w') as configfile:
+        config.write(configfile)
+    return data['loc']
+
+# are we updating from IP address? Or using fixed address?
+if varFixedLocation == "false":
+    latlon = ipInfo()
+else:
+    latlon = config['weather']['location']
+lat,lon = latlon.split(",")
+
+
+# get weather data
+def getWeather():
+    iconmap = {
+        "01": "Clear Sky",
+        "02": "Few Clouds",
+        "03": "Scattered Clouds",
+        "04": "Broken Clouds",
+        "09": "Rain Showers",
+        "10": "Rain",
+        "11": "Thunderstorms",
+        "13": "Snow",
+        "50": "Mist"
+    }
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, varApiKey)
+    weatherresponse = requests.get(url)
+    weatherdata = json.loads(weatherresponse.text)
+    icon_arch = (weatherdata["current"]["weather"][0]["icon"])
+    icon_strip = re.sub('[dn]', '', icon_arch)
+    current_temp = weatherdata["current"]["temp"]
+    current_cond = iconmap.get(icon_strip, icon_strip)
+    if varDebug == "true":
+        print ("Api key: ", varApiKey)
+        print("Current Temperature: ", weatherdata["current"]["temp"])
+        print("Current Icon: ", weatherdata["current"]["weather"][0]["icon"])
+        print ("The icon text is: ", iconmap.get(icon_strip, icon_strip))
+        print ("Seconds until next refresh: ", varInterval)
+    return current_temp, current_cond;
+
+def panel_pihole():
+    y = top
+    draw.text((x, y), IP, font=font, fill="#FFFF00")
+    y += font.getsize(IP)[1]
+    draw.text((x, y), "hostname: {}".format(str(HOST)), font=font, fill="#FFFF00")
+    y += font.getsize(HOST)[1]
+    draw.text((x, y), "Ads Blocked: {}".format(str(ADSBLOCKED)), font=font, fill="#00FF00")
+    y += font.getsize(str(ADSBLOCKED))[1]
+    draw.text((x, y), "Clients: {}".format(str(CLIENTS)), font=font, fill="#0000FF")
+    y += font.getsize(str(CLIENTS))[1]
+    draw.text((x, y), "DNS Queries: {}".format(str(DNSQUERIES)), font=font, fill="#FF00FF")
+    y += font.getsize(str(DNSQUERIES))[1]
+    draw.text((x, y), "Blocking: {}".format(str(STATUS)), font=font, fill="#FF00FF")
+    y += font.getsize(str(STATUS))[1]
+
+def panel_stats():
+    y = top
+    draw.text((x, y), IP, font=font, fill="#FFFF00")
+    y += font.getsize(IP)[1]
+    draw.text((x, y), CPU, font=font, fill="#FFFF00")
+    y += font.getsize(CPU)[1]
+    draw.text((x, y), MemUsage, font=font, fill="#00FF00")
+    y += font.getsize(MemUsage)[1]
+    draw.text((x, y), Disk, font=font, fill="#0000FF")
+    y += font.getsize(Disk)[1]
+    draw.text((x, y), "DNS Queries: {}".format(DNSQUERIES), font=font, fill="#FF00FF")
+
+def panel_weather(current_temp, current_cond):
+    y = top
+    draw.text((x, y), "Temperature: {}".format(str(current_temp)), font=font, fill="#FFFF00")
+    y += font.getsize(str(current_temp))[1]
+    draw.text((x, y), "Currently it is ", font=font, fill="#FFFF00")
+    y += font.getsize("Currently it is ")[1]
+    draw.text((x, y), current_cond, font=font, fill="#00FF00")
+    y += font.getsize(str(current_cond))[1]
+    
+
+def panel_update():
+    y = top
+    draw.text((x, y), "UPDATE CutePiHole?", font=font, fill="#FFFF00")
+    y += font.getsize("UPDATE CutePiHole?")[1]
+    draw.text((x, y), "Press Middle", font=font, fill="#FFFF00")
+    y += font.getsize("Press Middle")[1]
+    draw.text((x, y), "Click to", font=font, fill="#00FF00")
+    y += font.getsize("Click to")[1]
+    draw.text((x, y), "Update", font=font, fill="#0000FF")
+    y += font.getsize("Update")[1]
+
+
+# LCD Setup
+
+# 240x240 display with hardware SPI:
+disp = LCD_1in44.LCD()
+Lcd_ScanDir = LCD_1in44.SCAN_DIR_DFT  #SCAN_DIR_DFT = D2U_L2R
+disp.LCD_Init(Lcd_ScanDir)
+disp.LCD_Clear()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = 128
+height = 128
+image = Image.new('RGB', (width, height))
+#rotation = 90 # need to figure this out
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.LCD_ShowImage(image,0,0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 12)
+backlight = 1
+
+# main event
+while True:
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here:
+    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    cmd = "hostname -I | cut -d\' \' -f1"
+    IP = "IP: "+subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "hostname | tr -d \'\\n\'"
+    HOST = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%d GB  %s\", $3,$2,$5}'"
+    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk \'{printf \"CPU Temp: %.1f C\", $(NF-0) / 1000}\'" # pylint: disable=line-too-long
+
+    # Pi Hole data!
+    try:
+        r = requests.get(varPiholeApiUrl)
+        data = json.loads(r.text)
+        DNSQUERIES = data['dns_queries_today']
+        ADSBLOCKED = data['ads_blocked_today']
+        CLIENTS = data['unique_clients']
+        STATUS = data['status']
+    except KeyError:
+        time.sleep(1)
+        continue
+
+    # Weather data!
+    if varInterval == 0:
+        try:
+            varInterval = config['weather'].getint('interval')
+            current_temp, current_cond = getWeather()
+        except KeyError:
+            time.sleep(1)
+            continue
+    else:
+        varInterval -= 1
+        if varDebug == "true":
+            print ("Seconds until next refresh: ", varInterval)
+
+    y = top
+    
+    if GPIO.input(KEY_UP_PIN) == 0:
+        screenid += 1
+        if screenid >= 4:
+            screenid = 1
+
+    if GPIO.input(KEY_DOWN_PIN) == 0:
+        screenid -= 1
+        if screenid <= 0:
+            screenid = 3
+
+    if GPIO.input(KEY_LEFT_PIN) == 0:
+        if varUpdatePanel == "true":
+            screenid = 4
+
+    #if GPIO.input(KEY_PRESS_PIN) == 0:
+        #if screenid == 4:
+            #do update
+
+    if GPIO.input(KEY1_PIN) == 0: 
+        panel_stats()
+    else:
+        if screenid == 1:
+            panel_pihole()
+        elif screenid == 2:
+            panel_weather(current_temp, current_cond)
+        elif screenid == 3:
+            panel_stats()
+        elif screenid == 4:
+            panel_update()
+        else:
+            panel_pihole()
+
+    
+    if GPIO.input(KEY2_PIN) == 0: 
+        try:
+            r = requests.get(disable_url)
+            if varDebug == "true":
+                print ("Key 2 Pushed - Disabling PiHole for 5m")
+        except:
+            time.sleep(1)
+
+    if GPIO.input(KEY3_PIN) == 0: 
+        if backlight == 1:
+            GPIO.output(LCD_Config.LCD_BL_PIN,GPIO.LOW)
+            backlight = 0
+            if varDebug == "true":
+                print ("Key 3 Pushed - Disabling LCD")
+        else:
+            GPIO.output(LCD_Config.LCD_BL_PIN,GPIO.HIGH)
+            backlight = 1
+            if varDebug == "true":
+                print ("Key 3 Pushed - Enabling LCD")
+    
+    # Display image.
+    signal.signal(signal.SIGINT, signal_handler)
+    #signal.pause()
+    disp.LCD_ShowImage(image,0,0)
+    time.sleep(.1)
+# except:
+    # print("except")
+    # GPIO.cleanup()
